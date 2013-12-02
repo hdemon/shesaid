db1666fed29b0e8100d3dbde66329cecfa7a4e47
Masami Yonehara <zeitdiebe@gmail.com>
2013-12-03 03:53:41 +0900

    [from now] Tue Dec  3 03:53:41 JST 2013
    diff --git a/app.rb b/app.rb
    index e28bf9b..7aac3e1 100644
    --- a/app.rb
    +++ b/app.rb
    @@ -1,13 +1,41 @@
     require "./lib/shesaid"
    +require "rmagick"
    
     get '/' do
    -  images = SheSaid::Image.where(id: SheSaid::ImageCharacter.where(character_id: character_id))
    -  image = images.first(offset: rand(images.count))
    -                        # .first(offset: rand(SheSaid::Image.count))
       headers 'Content-Type' => "image/jpeg"
    -  image[:blob]
    +  set_baloon(material: image[:blob], sentence: params[:sentence])
     end
    
     def character_id
    -  SheSaid::Character.where(name: params[:character]).shift.id
    +  if params[:character].present?
    +    SheSaid::Character.where(name: params[:character]).shift.id
    +  else
    +    SheSaid::Character.first(offset: rand(SheSaid::Character.count)).id
    +  end
     end
    +
    +def image
    +  images = SheSaid::Image.where(id: SheSaid::ImageCharacter.where(character_id: character_id))
    +  image = images.first(offset: rand(images.count))
    +end
    +
    +def set_baloon(args)
    +  canvas = Magick::Image.from_blob(args[:material]).shift
    +  size = 30
    +  text = "あいうえおアリスLGTM陽子"
    +  draw = Magick::Draw.new
    +  draw.font "ipamp.ttf"
    +  draw.font_weight 900
    +
    +  draw.annotate(canvas, 0, 0, 50, 100 + size, text) do
    +    self.font = 'Verdana-Bold'
    +    self.fill = '#FFFFFF'
    +    self.align = Magick::LeftAlign
    +    self.stroke = 'transparent'
    +    self.pointsize = 30
    +    self.text_antialias = true
    +    self.kerning = 1
    +  end
    +  canvas.to_blob
    +
    +end
    \ No newline at end of file

--- app.rb
+++ app.rb
@@ -1,13 +1,41 @@
 require "./lib/shesaid"
+require "rmagick"
 
 get '/' do
-  images = SheSaid::Image.where(id: SheSaid::ImageCharacter.where(character_id: character_id))
-  image = images.first(offset: rand(images.count))
-                        # .first(offset: rand(SheSaid::Image.count))
   headers 'Content-Type' => "image/jpeg"
-  image[:blob]
+  set_baloon(material: image[:blob], sentence: params[:sentence])
 end
 
 def character_id
-  SheSaid::Character.where(name: params[:character]).shift.id
+  if params[:character].present?
+    SheSaid::Character.where(name: params[:character]).shift.id
+  else
+    SheSaid::Character.first(offset: rand(SheSaid::Character.count)).id
+  end
+end
+
+def image
+  images = SheSaid::Image.where(id: SheSaid::ImageCharacter.where(character_id: character_id))
+  image = images.first(offset: rand(images.count))
+end
+
+def set_baloon(args)
+  canvas = Magick::Image.from_blob(args[:material]).shift
+  size = 30
+  text = "あいうえおアリスLGTM陽子"
+  draw = Magick::Draw.new
+  draw.font "ipamp.ttf"
+  draw.font_weight 900
+
+  draw.annotate(canvas, 0, 0, 50, 100 + size, text) do
+    self.font = 'Verdana-Bold'
+    self.fill = '#FFFFFF'
+    self.align = Magick::LeftAlign
+    self.stroke = 'transparent'
+    self.pointsize = 30
+    self.text_antialias = true
+    self.kerning = 1
+  end
+  canvas.to_blob
+
 end
