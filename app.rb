fba5618da31ef0eda747df311790cc17ec1e9d6c
Masami Yonehara <zeitdiebe@gmail.com>
2013-12-04 19:05:35 +0900

    enable to draw face box

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require "rmagick"
 require 'rvg/rvg'
 include Magick
 
+
 get '/' do
   headers 'Content-Type' => "image/jpeg"
   baloon_overprinted_image(material: image, sentence: params[:sentence])
@@ -28,16 +29,37 @@ def baloon_overprinted_image(args)
   base_width = args[:material].width
   base_height = args[:material].height
 
-  baloon_height = args[:material].height / 4
+  baloon_height = args[:material].height / 3.7
   baloon_width = baloon_height / 1.6
 
   baloon_obj = Baloon.new(base_width: base_width, base_height: base_height,
                           x: base_width / 2, y: base_height / 2,
                           width: baloon_width, height: baloon_height,
                           phrase: "進捗\nどうですか")
-                      .create
+                     .create
 
   overprinted_obj = image_obj.composite baloon_obj, CenterGravity, 0, 0, OverCompositeOp
+  # overprinted_obj.to_blob
+
+  face = args[:material][:face_data].first["face"]
+
+  face_box = FaceBox.new(start_x: face["x"], start_y: face["y"], width: face["width"], height: face["height"])
+  face_box.set_base(base_width, base_height)
+  face_box_obj = face_box.create
+  overprinted_obj = overprinted_obj.composite face_box_obj, CenterGravity, 0, 0, OverCompositeOp
+
+  # baloon_area_box = FaceBox.new(
+  #   start_x: @start_x - face_box[:width] * PADDING[:left],
+  #   start_y: @start_y - face_box[:height] * PADDING[:top],
+  #   end_x: (@start_x + @width) + face_box[:width] * PADDING[:right],
+  #   end_y: (@start_y + @height) + face_box[:height] * PADDING[:bottom] )
+
+  # baloon_area_box.set_base(base_width, base_height)
+  # baloon_area_box_obj = baloon_area_box.create
+  # overprinted_obj = overprinted_obj.composite baloon_area_box_obj, CenterGravity, 0, 0, OverCompositeOp
+ # face_box = FaceBox.new(base_width: base_width, base_height: base_height, face: face).create
+  # baloon_area_obj = BaloonArea.new(base_width: base_width, base_height: base_height, face: face).create
+
   overprinted_obj.to_blob
 end
 
--- lib/shesaid/baloon.rb
+++ lib/shesaid/baloon.rb
@@ -1,6 +1,82 @@
 require "rmagick"
 require 'rvg/rvg'
+require 'pp'
 include Magick
+
+
+class Box
+  def initialize(args)
+    @start_x = args[:start_x]
+    @start_y = args[:start_y]
+    @width = args[:width]
+    @height = args[:height]
+    @end_x = args[:end_x] || @start_x + @width
+    @end_y = args[:end_y] || @start_y + @height
+  end
+
+  def set_base(base_width, base_height)
+    @base_width = base_width
+    @base_height = base_height
+  end
+
+  def create
+    img = Magick::Image.new(@base_width, @base_height) {
+      self.background_color = "Transparent"
+    }
+    gc = Magick::Draw.new
+
+    gc.stroke("#000000")
+    gc.stroke_width(1)
+    gc.fill 'rgba(0,0,0,0)'
+    gc.rectangle(@start_x, @start_y, @end_x, @end_y)
+    gc.draw img
+    img.write("box.png")
+    Magick::Image.read('box.png').shift
+  end
+end
+
+
+class FaceBox < Box
+end
+
+
+class BaloonArea < Box
+  PADDING = { top: 0.2, right: 0.6, bottom: 0.4, left: 0.6 }
+
+  def initialize(args)
+    @base_width = args[:base_width]
+    @base_height = args[:base_height]
+    @face = args[:face]
+  end
+
+  def wrapper_box
+    @wrapper_box ||= {
+      start_x: @start_x - face_box[:width] * PADDING[:left],
+      start_y: @start_y - face_box[:height] * PADDING[:top],
+      width: @width,
+      height: @height,
+      end_x: (@start_x + @width) + face_box[:width] * PADDING[:right],
+      end_y: (@start_y + @height) + face_box[:height] * PADDING[:bottom],
+    }
+  end
+
+  def create
+    img = Magick::Image.new(@base_width, @base_height) {
+      self.background_color = "Transparent"
+    }
+    gc = Magick::Draw.new
+
+    gc.stroke("#000000")
+    gc.stroke_width(1)
+    gc.fill 'rgba(0,0,0,0)'
+    gc.rectangle(face_box[:start_x], face_box[:start_y], face_box[:end_x], face_box[:end_y])
+    gc.rectangle(wrapper_box[:start_x], wrapper_box[:start_y], wrapper_box[:end_x], wrapper_box[:end_y])
+    gc.draw img
+    img.write("baloon_area.png")
+    Magick::Image.read('baloon_area.png').shift
+  end
+end
+
 
 
 class Baloon
